9d984cfed50dbef36caaefde9a85bb20b8982ab7
RobotCorgi <scarson4@lesley.edu>
2022-04-14 15:35:06 -0400

    Part 9 Add PHP Variables, Functions, and Loops

--- footer.php
+++ footer.php
@@ -3,6 +3,6 @@
             <p>Footer area</p>
         </div>
         <div class="attribution">
-            <p><a href="https://storyset.com/work">Work illustrations by Storyset</a></p>
+            <p><a href="https://storyset.com/work">Work illustrations by Storyset</a><p>
         </div>
 </footer>
---
+++ images/part9.php
@@ -0,0 +1,48 @@
+<?php 
+ini_set(‘display_errors’,1);
+?>
+
+$firstName = "Jane";
+$lastName = "Doe";
+$age = 35;
+$married = true; 
+$childrenNames = array("Alice", "Bob");
+
+
+$fullName = $firstName . " " . $lastName;
+$dogYears = $age/7;
+$isAdult = $dogYears > 4;
+$isSettled = $isAdult and $married;
+
+function getFullName($firstName, $lastName) {
+    return $firstName . " " . $lastName;
+}
+
+$fullName = getFullName($firstName, $lastName);
+$fullName = getFullName("Alice", "Doe");
+
+function getIsSettled($age, $married) {
+    $dogYears = $age/7;
+    $isAdult = $dogYears > 4;
+    $isSettled = $isAdult and $married;
+    return $isSettled;
+}
+
+$isSettled = getIsSettled($age, $married);
+$isSettled = getIsSettled(5, false);
+
+echo $fullName;
+
+echo "<h1>Hello World</h1>"
+
+if ($isSettled) {
+    echo $fullName . " is settled.";
+} else {
+    echo $fullName . " is not settled.";
+}
+
+
+foreach($childrenNames as $childName) {
+    echo "<li>" . $childName . "</li>";
+}
+
